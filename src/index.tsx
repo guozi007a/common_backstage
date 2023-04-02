468d82517bde380769e43b2ceeeaf3146cbb27e0
guozi007a <1208001055@qq.com>
2023-04-02 20:58:01 +0800

    1

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from './router';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/common_backstage/'>
         <App />
     </BrowserRouter>
 )
