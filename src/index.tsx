706ac0695a0c942c72d7da898202c52e8e8e29e4
guozi007a <1208001055@qq.com>
2023-04-02 14:52:31 +0800

    setup router

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 import { createRoot } from 'react-dom/client';
-// import App from '@/App';
 import './i18n/config';
 import './styles/common.less';
 import 'antd/dist/reset.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import App from './router';
-import Home from './pages/home';
-import Login from './pages/login';
+import { BrowserRouter } from 'react-router-dom';
+import App from './router';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-        </Routes>
+        <App />
     </BrowserRouter>
 )
--- src/router/ele.tsx
+++ src/router/ele.tsx
@@ -1,9 +1,14 @@
-import Home from '../pages/home';
 import { RouteObject } from 'react-router-dom';
+import Home from '../pages/home';
+import Login from '../pages/login';
 
 export const ele: RouteObject[] = [
     {
         path: '/',
         element: <Home />
+    },
+    {
+        path: '/login',
+        element: <Login />
     }
-]
+];
