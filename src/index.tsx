b4ac8f44d852e3e69ee90f8cd1e52c4eb1e9c036
guozi007a <1208001055@qq.com>
2023-04-02 21:04:05 +0800

    解决部署白屏问题

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,8 @@ import App from './router';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    // 配置basename，不然部署后白屏
+    // 配置basename，不然部署到gitee后白屏
+    // 部署服务器时，使用nginx配置，这里就不用配置basename了
     <BrowserRouter basename='/common_backstage/'>
         <App />
     </BrowserRouter>
