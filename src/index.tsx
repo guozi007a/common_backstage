9a1d1a44c8316187f1f48e594b44a0f5ae3a3c56
guozi007a <1208001055@qq.com>
2023-04-02 21:22:45 +0800

    配置环境

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import 'antd/dist/reset.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './router';
 
+console.log('env: ', process.env.NODE_ENV);
+
 const root = createRoot(document.getElementById('root'));
 root.render(
     // 配置basename，不然部署到gitee后白屏
     // 部署服务器时，使用nginx配置，这里就不用配置basename了
-    <BrowserRouter basename='/common_backstage/'>
+    <BrowserRouter basename={`${process.env.NODE_ENV === 'development' ? '' : '/common_backstage/'}`}>
         <App />
     </BrowserRouter>
 )
