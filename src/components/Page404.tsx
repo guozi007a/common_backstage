e6bbde5dc648bfa99514084f64722d4c5cdae5f4
wushijie <wushijie@melot.cn>
2023-04-03 11:03:35 +0800

    page 404

--- src/components/Page404.tsx
+++ src/components/Page404.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'antd';
 
 const Page404: FC = () => {
     
+    const navigate = useNavigate();
+
     return <>
-    
+        <h3>404</h3>
+        <Button type='primary' onClick={() => {navigate('/')}}>redirect to homepage</Button>
     </>
 }
 
