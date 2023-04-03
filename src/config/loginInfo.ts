4dc7d43aa117a3d1418a5b5514b9d0eb0a5261f4
guozi007a <1208001055@qq.com>
2023-04-03 23:52:12 +0800

    homepage

--- src/config/loginInfo.ts
+++ src/config/loginInfo.ts
@@ -1,2 +1,5 @@
-/** 储存用户登录信息的字段 */
+/** 
+ * 储存用户登录信息的字段
+ * 所有的登录信息都存储在USERINFO字段里
+ */
 export const USERINFO = 'USERINFO';
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,70 @@
-import { FC } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button } from 'antd';
+import React, { FC, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import { Layout, Menu, theme } from 'antd';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    HomeOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import './index.less';
+
+const { Header, Sider, Content } = Layout;
 
 const Home: FC = () => {
 
-    const navigate = useNavigate();
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
 
     return <>
-        <h3>Home</h3>
-        <Button type="primary" onClick={() => {navigate('/login')}}>to login page</Button>
+        <Layout>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="logo" />
+                <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                items={[
+                    {
+                        key: '1',
+                        icon: <HomeOutlined />,
+                        label: '首页',
+                    },
+                    {
+                        key: '2',
+                        icon: <VideoCameraOutlined />,
+                        label: 'nav 2',
+                    },
+                    {
+                        key: '3',
+                        icon: <UploadOutlined />,
+                        label: 'nav 3',
+                    },
+                ]}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header style={{ background: colorBgContainer }}>
+                {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                    className: 'trigger',
+                    onClick: () => setCollapsed(!collapsed),
+                })}
+                </Header>
+                <Content
+                style={{
+                    margin: '24px 16px',
+                    padding: 24,
+                    minHeight: '100vh',
+                    background: colorBgContainer,
+                }}
+                >
+                Content
+                </Content>
+            </Layout>
+        </Layout>
     </>
 }
 
