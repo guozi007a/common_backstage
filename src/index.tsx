9c01d55ebdc27eff07642c25d701bcaf85c7081b
wushijie <wushijie@melot.cn>
2023-04-03 10:30:48 +0800

    rebuild

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './router';
 import { Provider } from 'react-redux';
 import store from '@/store';
-// import PROFILE_ROOT_URL from '@/config/profileRootUrl';
+import PROFILE_ROOT_URL from '@/config/profileRootUrl';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter /* basename={`${process.env.NODE_ENV === 'development' ? '' : PROFILE_ROOT_URL}`} */>
+        <BrowserRouter basename={`${process.env.NODE_ENV === 'development' ? '' : PROFILE_ROOT_URL}`}>
             <App />
         </BrowserRouter>
     </Provider>
