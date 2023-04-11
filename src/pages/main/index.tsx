133ffaad2675ad7d5d46ab3c67f3c138a4d548ef
wushijie <wushijie@melot.cn>
2023-04-11 18:00:35 +0800

    echarts

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,95 @@
-import { FC } from 'react';
+/** 首页 */
+import { FC, useEffect, useRef } from 'react';
+import * as echarts from 'echarts/core';
+import {
+  BarChart,
+  // 系列类型的定义后缀都为 SeriesOption
+  BarSeriesOption,
+  LineChart,
+  LineSeriesOption
+} from 'echarts/charts';
+import {
+  TitleComponent,
+  // 组件类型的定义后缀都为 ComponentOption
+  TitleComponentOption,
+  TooltipComponent,
+  TooltipComponentOption,
+  GridComponent,
+  GridComponentOption,
+  // 数据集组件
+  DatasetComponent,
+  DatasetComponentOption,
+  // 内置数据转换器组件 (filter, sort)
+  TransformComponent
+} from 'echarts/components';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+
+type ECOption = echarts.ComposeOption<
+    | BarSeriesOption
+    | LineSeriesOption
+    | TitleComponentOption
+    | TooltipComponentOption
+    | GridComponentOption
+    | DatasetComponentOption
+>;
+
+    // 注册必须的组件
+echarts.use([
+    TitleComponent,
+    TooltipComponent,
+    GridComponent,
+    DatasetComponent,
+    TransformComponent,
+    BarChart,
+    LineChart,
+    LabelLayout,
+    UniversalTransition,
+    CanvasRenderer
+]);
 
 const Main: FC = () => {
-    return <h3>main</h3>
+
+    const chartRef = useRef(null);
+
+    const chartOptions: ECOption = {
+        title: {
+            text: 'Echarts 入门'
+        },
+        tooltip: {},
+        xAxis: {
+            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+            {
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }
+        ]
+    };
+
+    useEffect(() => { 
+        
+        const myChart = echarts.init(chartRef.current);
+        myChart.setOption(chartOptions);
+
+        const handleResize = () => {
+            myChart.resize();
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+
+    return <>
+        <h3>首页</h3>
+        <div ref={chartRef} style={{width: '50%', height: 300}}></div>
+    </>
 }
 
 export default Main;
