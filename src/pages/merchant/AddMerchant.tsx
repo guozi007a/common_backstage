cd3c8dc45e4f5648c074de51b996f43ceaa58119
guozi007a <1208001055@qq.com>
2023-04-17 22:19:48 +0800

    添加商户

--- src/pages/merchant/AddMerchant.tsx
+++ src/pages/merchant/AddMerchant.tsx
@@ -1,5 +1,26 @@
 import { FC } from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input, message, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+import './index.less';
+
+const props: UploadProps = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+        authorization: 'authorization-text',
+    },
+    onChange(info) {
+        if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+        }
+    },
+};
 
 const AddMerchant: FC = () => {
 
@@ -11,7 +32,7 @@ const AddMerchant: FC = () => {
         console.log('Failed:', errorInfo);
     };
 
-    return <>
+    return <div className='add_merchant_wrap'>
         <Form
             name="add_merchant"
             labelCol={{ span: 8 }}
@@ -22,12 +43,16 @@ const AddMerchant: FC = () => {
             onFinishFailed={onFinishFailed}
             autoComplete="off"
         >
+            <Upload {...props}>
+                <Button icon={<UploadOutlined />}>上传营业执照图片</Button>
+            </Upload>
+
             <Form.Item
                 label="公司名称"
                 name="mechant_name"
                 rules={[{ required: true, message: 'Please input your company name!' }]}
             >
-            <Input />
+                <Input />
             </Form.Item>
 
             <Form.Item
@@ -35,20 +60,48 @@ const AddMerchant: FC = () => {
                 name="licenseid"
                 rules={[{ required: true, message: 'Please input your licenseid!' }]}
             >
-            <Input />
+                <Input />
             </Form.Item>
 
-            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                <Checkbox>Remember me</Checkbox>
+            <Form.Item
+                label="公司法人代表"
+                name="legal"
+                rules={[{ required: true, message: 'Please input your legal!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="联系人"
+                name="contacts"
+                rules={[{ required: true, message: 'Please input your contacts!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="联系方式"
+                name="phone"
+                rules={[{ required: true, message: 'Please input your phone number!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="公司地址"
+                name="address"
+                rules={[{ required: true, message: 'Please input your address!' }]}
+            >
+                <Input />
             </Form.Item>
 
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                 <Button type="primary" htmlType="submit">
-                    Submit
+                    确认添加商户
                 </Button>
             </Form.Item>
         </Form>
-    </>
+    </div>
 }
 
 export default AddMerchant;
--- src/pages/merchant/index.tsx
+++ src/pages/merchant/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
+import './index.less';
 
 const Merchant: FC = () => {
 
